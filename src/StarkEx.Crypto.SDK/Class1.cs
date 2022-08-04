a1c9a56562a2c626ae4df027a565433b42cecca4
Jose Afonso <jose@threesigma.xyz>
2022-08-04 11:24:13 +0100

    feat: Initial project bootstrapping

---
+++ src/StarkEx.Crypto.SDK/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace StarkEx.Crypto.SDK;
+
+public class Class1
+{
+}
---
+++ tests/StarkEx.Crypto.SDK.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace StarkEx.Crypto.SDK.Tests;
+
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
