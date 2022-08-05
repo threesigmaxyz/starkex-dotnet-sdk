b6616678f06b973d869df73f9301a6c7b802939e
Jose Afonso <jose@threesigma.xyz>
2022-08-05 19:03:20 +0100

    feat: Add StarkEx signer

---
+++ src/StarkEx.Crypto.SDK/Signing/StarkExSigner.cs
@@ -0,0 +1,73 @@
+﻿namespace StarkEx.Crypto.SDK.Signing;
+
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Utilities;
+using StarkEx.SDK.Models;
+
+// TODO Assert ranges in whole solution!
+public class StarkExSigner : IStarkExSigner
+{
+    private readonly StarkCurve starkCurve;
+
+    public StarkExSigner(StarkCurve starkCurve)
+    {
+        this.starkCurve = starkCurve;
+    }
+
+    // TODO Segregate Models into a separate sdk to prevent circular dependencies
+    public SignatureModel SignMessage(string messageHash, string privateKey)
+    {
+        var messageHashAsBigInteger = new BigInteger(messageHash, 16);
+        var privateKeyAsBigInteger = new BigInteger(privateKey, 16);
+
+        var hMacDsaKCalculator = new HMacDsaKCalculator(new Sha256Digest());
+        var signer = new ECDsaSigner(hMacDsaKCalculator);
+        signer.Init(true, starkCurve.CreatePrivateKeyParams(privateKeyAsBigInteger));
+        var signature = signer.GenerateSignature(ToByteArray(FixMessageLength(messageHashAsBigInteger)));
+
+        return new SignatureModel
+        {
+            R = signature[0].ToString(16),
+            S = signature[1].ToString(16),
+        };
+    }
+
+    public bool VerifySignature(string messageHash, string publicKey, SignatureModel signature)
+    {
+        var messageHashAsBigInteger = new BigInteger(messageHash, 16);
+        var publicKeyAsBigInteger = new BigInteger(publicKey, 16);
+
+        var signer = new ECDsaSigner();
+        var publicKeyParameters = starkCurve.CreatePublicKeyParams(publicKeyAsBigInteger);
+        signer.Init(false, publicKeyParameters);
+
+        return signer.VerifySignature(
+            ToByteArray(FixMessageLength(messageHashAsBigInteger)),
+            new BigInteger(signature.R, 16),
+            new BigInteger(signature.S, 16));
+    }
+
+    private static byte[] ToByteArray(BigInteger value)
+    {
+        var signedValue = value.ToByteArray();
+        return signedValue[0] != 0x00 ?
+            signedValue :
+            Arrays.CopyOfRange(signedValue, 1, signedValue.Length);
+    }
+
+    private static BigInteger FixMessageLength(BigInteger messageHash)
+    {
+        var hashHex = messageHash.ToString(16);
+
+        return hashHex.Length switch
+        {
+            <= 62 => messageHash,
+
+            // In this case delta will be 4, so we perform a shift-left of 4 bits.
+            63 => messageHash.ShiftLeft(4),
+            _ => throw new ArgumentException($"Invalid message hash with length {hashHex.Length}", nameof(messageHash)),
+        };
+    }
+}
---
+++ tests/StarkEx.Crypto.SDK.Tests/Signing/StarkExSignerTests.cs
@@ -0,0 +1,97 @@
+﻿namespace StarkEx.Crypto.SDK.Tests.Signing;
+
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using StarkEx.Crypto.SDK.Signing;
+using StarkEx.SDK.Models;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+public class StarkExSignerTests
+{
+    [Theory]
+    [InlineData(
+        "c465dd6b1bbffdb05442eb17f5ca38ad1aa78a6f56bf4415bdee219114a47",
+        "5f496f6f210b5810b2711c74c15c05244dad43d18ecbbdbe6ed55584bc3b0a2",
+        "4e8657b153787f741a67c0666bad6426c3741b478c8eaa3155196fc571416f3")]
+    [InlineData(
+        "00c465dd6b1bbffdb05442eb17f5ca38ad1aa78a6f56bf4415bdee219114a47",
+        "5f496f6f210b5810b2711c74c15c05244dad43d18ecbbdbe6ed55584bc3b0a2",
+        "4e8657b153787f741a67c0666bad6426c3741b478c8eaa3155196fc571416f3")]
+    [InlineData(
+        "c465dd6b1bbffdb05442eb17f5ca38ad1aa78a6f56bf4415bdee219114a47a",
+        "233b88c4578f0807b4a7480c8076eca5cfefa29980dd8e2af3c46a253490e9c",
+        "28b055e825bc507349edfb944740a35c6f22d377443c34742c04e0d82278cf1")]
+    public void SignMessage_InputsAreValid_SignatureIsSameAsExpected(
+        string messageHash,
+        string expectedR,
+        string expectedS)
+    {
+        // Arrange
+        var privateKey = "2dccce1da22003777062ee0870e9881b460a8b7eca276870f57c601f182136c";
+        var target = CreateStarkExSigner();
+        var expectedResult = new SignatureModel()
+        {
+            R = expectedR,
+            S = expectedS,
+        };
+
+        // Act
+        var result = target.SignMessage(
+            messageHash,
+            privateKey);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(
+        "c465dd6b1bbffdb05442eb17f5ca38ad1aa78a6f56bf4415bdee219114a47",
+        "0400499f65ae2f71d5298d2d88823b2e5e19596a71aac1984710479e406a00243904745865467631492cf6ecc433a3cf4ecc580d698097d6b738ad8f3da7c4d66c",
+        "5f496f6f210b5810b2711c74c15c05244dad43d18ecbbdbe6ed55584bc3b0a2",
+        "4e8657b153787f741a67c0666bad6426c3741b478c8eaa3155196fc571416f3")]
+    [InlineData(
+        "00c465dd6b1bbffdb05442eb17f5ca38ad1aa78a6f56bf4415bdee219114a47",
+        "0400499f65ae2f71d5298d2d88823b2e5e19596a71aac1984710479e406a00243904745865467631492cf6ecc433a3cf4ecc580d698097d6b738ad8f3da7c4d66c",
+        "5f496f6f210b5810b2711c74c15c05244dad43d18ecbbdbe6ed55584bc3b0a2",
+        "4e8657b153787f741a67c0666bad6426c3741b478c8eaa3155196fc571416f3")]
+    [InlineData(
+        "c465dd6b1bbffdb05442eb17f5ca38ad1aa78a6f56bf4415bdee219114a47a",
+        "0400499f65ae2f71d5298d2d88823b2e5e19596a71aac1984710479e406a00243904745865467631492cf6ecc433a3cf4ecc580d698097d6b738ad8f3da7c4d66c",
+        "233b88c4578f0807b4a7480c8076eca5cfefa29980dd8e2af3c46a253490e9c",
+        "28b055e825bc507349edfb944740a35c6f22d377443c34742c04e0d82278cf1")]
+    [InlineData(
+        "7465dd6b1bbffdb05442eb17f5ca38ad1aa78a6f56bf4415bdee219114a47a1",
+        "0400499f65ae2f71d5298d2d88823b2e5e19596a71aac1984710479e406a00243904745865467631492cf6ecc433a3cf4ecc580d698097d6b738ad8f3da7c4d66c",
+        "b6bee8010f96a723f6de06b5fa06e820418712439c93850dd4e9bde43ddf",
+        "1a3d2bc954ed77e22986f507d68d18115fa543d1901f5b4620db98e2f6efd80")]
+    public void VerifySignature_InputsAreValid_SignatureIsValidated(
+        string messageHash,
+        string publicKey,
+        string signatureR,
+        string signatureS)
+    {
+        // Arrange
+        var target = CreateStarkExSigner();
+        var signature = new SignatureModel
+        {
+            R = signatureR,
+            S = signatureS,
+        };
+
+        // Act
+        var result = target.VerifySignature(
+            messageHash,
+            publicKey,
+            signature);
+
+        // Assert
+        result.Should().Be(true);
+    }
+
+    private static IStarkExSigner CreateStarkExSigner()
+    {
+        return new StarkExSigner(new StarkCurve());
+    }
+}
