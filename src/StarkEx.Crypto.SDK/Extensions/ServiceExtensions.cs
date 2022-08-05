c7debe06d85291c74e6cce33032447c3c466e237
Jose Afonso <jose@threesigma.xyz>
2022-08-05 19:03:50 +0100

    feat: Add ServiceCollection extension

---
+++ src/StarkEx.Crypto.SDK/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace StarkEx.Crypto.SDK.Extensions;
+
+using Microsoft.Extensions.DependencyInjection;
+using StarkEx.Crypto.SDK.Hashing;
+using StarkEx.Crypto.SDK.Signing;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddStarkExCryptoUtils(this IServiceCollection services)
+    {
+        services.AddSingleton<IPeadersonHash, PeadersonHash>();
+        services.AddSingleton<ISpotTradingMessageHasher, SpotTradingMessageHasher>();
+        services.AddSingleton<IStarkExSigner, StarkExSigner>();
+
+        return services;
+    }
+}
