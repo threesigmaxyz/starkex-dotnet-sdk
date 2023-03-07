1bfba280ae52088906b80d8d85f333025ace8372
Jose Afonso <104689221+JoseAfonsoThreeSigma@users.noreply.github.com>
2023-03-07 17:05:16 +0000

    feat: add stark keys derivation methods
    MINOR

---
+++ src/StarkEx.Commons.SDK/Models/StarkAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace StarkEx.Commons.SDK.Models;
+
+/// <summary>
+///     Keys for a Stark Account.
+/// </summary>
+public class StarkAccount
+{
+    /// <summary>
+    ///     Gets or sets the public key for the Stark Account.
+    /// </summary>
+    public string PublicKey { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets the private key for the Stark Account.
+    /// </summary>
+    public string PrivateKey { get; set; }
+}
